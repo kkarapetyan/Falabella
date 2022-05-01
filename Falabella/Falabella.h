1878022c01f02dfed68ffb6cce334acd3b37e092
kkarapetyan <karapetyankarine87@gmail.com>
2022-05-01 10:26:47 -0400

    Add Falabella project

---
+++ Falabella/Falabella.h
@@ -0,0 +1,18 @@
+//
+//  Falabella.h
+//  Falabella
+//
+//  Created by Karine Karapetyan on 01-05-22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Falabella.
+FOUNDATION_EXPORT double FalabellaVersionNumber;
+
+//! Project version string for Falabella.
+FOUNDATION_EXPORT const unsigned char FalabellaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Falabella/PublicHeader.h>
+
+
