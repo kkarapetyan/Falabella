5b7bb5d82cf4b6271e1aeaa36b248b63f49ba672
kkarapetyan <karapetyankarine87@gmail.com>
2022-05-01 14:49:58 -0400

    Added Constants header

--- Falabella/Falabella.h
+++ Falabella/Falabella.h
@@ -14,5 +14,6 @@ FOUNDATION_EXPORT double FalabellaVersionNumber;
 FOUNDATION_EXPORT const unsigned char FalabellaVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Falabella/PublicHeader.h>
+#import <Falabella/Constants>
 
 
