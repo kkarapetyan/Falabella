3e418bcc5711c0050847c4a2201ce7b5dfff56ab
kkarapetyan <karapetyankarine87@gmail.com>
2022-05-02 08:34:28 -0400

    Test xib load

--- Falabella/Falabella.swift
+++ Falabella/Falabella.swift
@@ -14,10 +14,42 @@ public final class Falabella {
         
     }
     
-   public func getCurrentViewController() -> UIViewController {
+   public func getCurrentViewController() -> UIViewController? {
        print ("Modul is working")
-       let bundle = Bundle(for: Falabella.self)
-       return UserDefaultsManager.shared.isLoggedIn() ? AttributesViewController(nibName: Constant.NibNames.attributes, bundle: bundle) : LoginViewController(nibName: Constant.NibNames.login, bundle: bundle)
+//       let bundle = Bundle(for: Falabella.self)
+//
+//
+//       let podBundle = Bundle(for:LoginViewController.self)
+//           if let bundleURL = podBundle.url(forResource: "Falabella", withExtension: "bundle") {
+//               if let bundle = Bundle(url: bundleURL) {
+//                   let cellNib = UINib(nibName: Constant.NibNames.login, bundle: bundle)
+//
+//               } else {
+//                   assertionFailure("Could not load the bundle")
+//               }
+//           } else {
+//               assertionFailure("Could not create a path to the bundle")
+//           }
+       
+       
+       
+       if UserDefaultsManager.shared.isLoggedIn() {
+           
+           let frameworkBundle = Bundle(for: LoginViewController.self)
+           let bundleURL = frameworkBundle.resourceURL?.appendingPathComponent("Falabella.bundle")
+           let resourceBundle = Bundle(url: bundleURL!)
+           
+          return  LoginViewController(nibName: Constant.NibNames.login, bundle: resourceBundle)
+           
+       } else {
+           let frameworkBundle = Bundle(for: AttributesViewController.self)
+           let bundleURL = frameworkBundle.resourceURL?.appendingPathComponent("Falabella.bundle")
+           let resourceBundle = Bundle(url: bundleURL!)
+           
+          return AttributesViewController(nibName: Constant.NibNames.attributes, bundle: resourceBundle)
+       }
+    
+//       return UserDefaultsManager.shared.isLoggedIn() ? AttributesViewController(nibName: Constant.NibNames.attributes, bundle: bundle) : LoginViewController(nibName: Constant.NibNames.login, bundle: bundle)
        
 //       let vc = UIViewController
 //       vc = UserDefaultsManager.shared.isLoggedIn() ? AttributesViewController(nibName: Constant.NibNames.attributes, bundle: nil) : LoginViewController(nibName: Constant.NibNames.login, bundle: nil)
