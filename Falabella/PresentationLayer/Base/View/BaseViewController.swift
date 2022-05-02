19860a8982a1f853b886312f39b3383ee831517b
kkarapetyan <karapetyankarine87@gmail.com>
2022-05-02 09:21:08 -0400

    Test xib load

--- Falabella/PresentationLayer/Base/View/BaseViewController.swift
+++ Falabella/PresentationLayer/Base/View/BaseViewController.swift
@@ -15,9 +15,12 @@ class BaseViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    func getBundleURL(myClass: AnyClass) -> URL? {
+    func getBundle(myClass: AnyClass) -> Bundle? {
+        
         let frameworkBundle = Bundle(for: myClass.self as! AnyClass)
-        return frameworkBundle.resourceURL?.appendingPathComponent("Falabella.bundle")
+        let bundleURL =  frameworkBundle.resourceURL?.appendingPathComponent("Falabella.bundle")
+        guard let url = bundleURL else { return nil}
+        return Bundle(url: url)
     }
 
 }
