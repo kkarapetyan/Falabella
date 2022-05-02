9920638f6ee0cf0edd4d50be89051e774f3ccd2a
kkarapetyan <karapetyankarine87@gmail.com>
2022-05-02 11:08:01 -0400

    Test xib load

--- Falabella/PresentationLayer/Login/View/LoginViewController.swift
+++ Falabella/PresentationLayer/Login/View/LoginViewController.swift
@@ -18,6 +18,13 @@ class LoginViewController: BaseViewController {
     
     lazy var presenter = LoginPresenter(delegate: self)
 
+    public init() {
+        super.init(nibName: Constant.NibNames.login, bundle: Bundle(for: LoginViewController.self))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: -- Life cycle
     override func viewDidLoad() {
