3222431872dae22104858f61a114cbf35986d76d
kkarapetyan <karapetyankarine87@gmail.com>
2022-05-02 09:30:57 -0400

    Test xib load

--- Falabella/PresentationLayer/Attributes/Views/Cell/AttributeTableViewCell.swift
+++ Falabella/PresentationLayer/Attributes/Views/Cell/AttributeTableViewCell.swift
@@ -10,17 +10,16 @@ import UIKit
 class AttributeTableViewCell: UITableViewCell {
     
     static let identifier = "AttributeTableViewCell"
+    
     static func nib() -> UINib {
-        static func nib() -> UINib {
-            let frameworkBundle = Bundle(for: AttributeTableViewCell.self)
-            let bundleURL = frameworkBundle.resourceURL?.appendingPathComponent("Falabella.bundle")
-            guard let url = bundleURL else {
-                return UINib(nibName: identifier, bundle: nil)
-            }
-            return UINib(nibName: identifier, bundle: Bundle(url: url))
+        let frameworkBundle = Bundle(for: AttributeTableViewCell.self)
+        let bundleURL = frameworkBundle.resourceURL?.appendingPathComponent("Falabella.bundle")
+        guard let url = bundleURL else {
+            return UINib(nibName: identifier, bundle: nil)
         }
+        return UINib(nibName: identifier, bundle: Bundle(url: url))
     }
-
+    
     @IBOutlet weak var mNameLb: UILabel!
     @IBOutlet weak var mValueLb: UILabel!
     
